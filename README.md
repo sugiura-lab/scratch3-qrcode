af7e0d94d85ec028cfd984347b82dbe698e8fa50
manabu-s <1431162+manabu-s@users.noreply.github.com>
2020-05-12 18:22:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# scratch3-qrcode
+
+## Installation
+This requires you to have Git and Node.js installed.
+
+```bash
+# set up scratch-gui
+git clone --depth 1 https://github.com/LLK/scratch-gui.git
+cd scratch-gui
+npm install
+
+# set up QR code extension
+git clone https://github.com/sugiura-lab/scratch3-qrcode.git
+sh scratch3-qrcode/install.sh
+```
+
+## Running
+Open a Command Prompt or Terminal in the repository and run:
+```bash
+npm start
+```
+Then go to http://localhost:8601/ - the playground outputs the default GUI component
+
+## Release Notes
+* 2020.5.12 v1.0
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+LF=$(printf '\\\012_')
+LF=${LF%_}
+REPOSITORY_NAME="scratch3-qrcode"
+EXTENSION_NAME="QRコード"
+EXTENSION_ID="qrcode"
+COLLABORATOR="Sugiura Lab"
+EXTENSION_DESCRIPTION="QRコードを読み取ります。QRコードは株式会社デンソーウェーブの登録商標です。"
+
+cd node_modules/scratch-vm
+npm install encoding-japanese@1.0.30
+npm install jsqr@1.3.1
+cd ../../
+
+mkdir -p node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}
+
+cp ${REPOSITORY_NAME}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/
+
+mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
+sed -e "s|class ExtensionManager {$|builtinExtensions['${EXTENSION_ID}'] = () => require('../extensions/scratch3_${EXTENSION_ID}');${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
+
+mkdir -p src/lib/libraries/extensions/${EXTENSION_ID}
+cp ${REPOSITORY_NAME}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}.png src/lib/libraries/extensions/${EXTENSION_ID}/
+cp ${REPOSITORY_NAME}/scratch-gui/src/lib/libraries/extensions/${EXTENSION_ID}/${EXTENSION_ID}-small.svg src/lib/libraries/extensions/${EXTENSION_ID}/
+mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
+DESCRIPTION="\
+    {${LF}\
+        name: '${EXTENSION_NAME}',${LF}\
+        extensionId: '${EXTENSION_ID}',${LF}\
+        collaborator: '${COLLABORATOR}',${LF}\
+        iconURL: ${EXTENSION_ID}IconURL,${LF}\
+        insetIconURL: ${EXTENSION_ID}InsetIconURL,${LF}\
+        description: '${EXTENSION_DESCRIPTION}',${LF}\
+        featured: true${LF}\
+    },"
+sed -e "s|^export default \[$|import ${EXTENSION_ID}IconURL from './${EXTENSION_ID}/${EXTENSION_ID}.png';${LF}import ${EXTENSION_ID}InsetIconURL from './${EXTENSION_ID}/${EXTENSION_ID}-small.svg';${LF}${LF}export default [${LF}${DESCRIPTION}|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
